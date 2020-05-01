f64c46f712df3983a345f8952d83f5561df0614e
Sandertv <st.ten.veldhuis@gmail.com>
2020-05-01 13:17:53 +0200

    v2: Variables and functions that don't exist and functions with too few arguments now resolve to NaN.

---
+++ v2/doc.go
@@ -0,0 +1,3 @@
+// Package formula implements very simple but fast formula parsing and evaluating. It uses the AST package
+// to parse expressions in the formula which it transforms to executable functions.
+package formula
